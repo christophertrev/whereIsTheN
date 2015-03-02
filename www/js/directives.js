56b727c7f34b6bfa840fa229ae80749fc6405cc0
Christopher Treviño <christophertrev@gmail.com>
2015-03-02 12:57:00 -0800

    (feat) Add happy face image to current position

--- www/js/directives.js
+++ www/js/directives.js
@@ -14,14 +14,26 @@ angular.module('starter.directives', [])
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map($element[0], mapOptions);
-  
+        var image = {
+          url: 'img/smile.png',
+           // This marker is 20 pixels wide by 32 pixels tall.
+          size: new google.maps.Size(20, 32),
+           // The origin for this image is 0,0.
+          origin: new google.maps.Point(0,0),
+           // The anchor for this image is the base of the flagpole at 0,32.
+          anchor: new google.maps.Point(0, 32)
+        };
+
+
+
         $scope.onCreate({map: map});
         navigator.geolocation.getCurrentPosition(function (pos){
           var currPosMarker = new google.maps.Marker({
           position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
           map: map,
-          title: 'Uluru (Ayers Rock)'
-          
+          title: 'Uluru (Ayers Rock)',
+          icon: image,
+
         });
         })
         // Stop the side bar from dragging when mousedown/tapdown on the map
